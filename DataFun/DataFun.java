1d5d4a07241cd36418d1e5ce630958053aabf820
Anshu Mukherjee <mukherja@wlhs.wlwv.k12.or.us>
2024-11-14 14:12:13 -0800

    update final

--- DataFun/DataFun.java
+++ DataFun/DataFun.java
@@ -2,111 +2,106 @@
 import java.util.Scanner;
 
 /**
-     * Description goes here
+     * Program that prints information about your favorite number and alphanumeric character.
      * @author [Anshu Mukherjee]
-     * @version [11/7/24]
+     * @version [11/14/24]
      */
 public class DataFun {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("What is your favorite integer?");
+        System.out.print("What is your favorite integer? ");
         int userNum = sc.nextInt();
         favNum(userNum);
-        System.out.print("What is your favorite character?");
+        System.out.print("\nWhat is your favorite alphanumeric character? ");
         char userChar = sc.next().charAt(0);
         favChar(userChar);
     }
 
 
     public static void favNum(int num) {
-        if (num >= 0){
-            System.out.println("\n" + num + " is a positive number,");
+        if (num >= 0) {
+            System.out.println("\n  " + num + " is a positive number,");
         } else {
-            System.out.println(num + " is a negative number,");
+            System.out.println("\n  " + num + " is a negative number,");
         }
 
-        if (num > 1000){
-            System.out.println("\nit is greater than 1000,");
-        } else if (num > 100){
-            System.out.println("\nit is greater than 100,");
-        } else if (num > 10){
-            System.out.println("\nit is greater than 10,");
+        if (num > 1000) {
+            System.out.println("\n  it is greater than 1000,");
+        } else if (num > 100) {
+            System.out.println("\n  it is greater than 100,");
+        } else if (num > 10) {
+            System.out.println("\n  it is greater than 10,");
         } else {
-            System.out.println("\nit is not greater than 10,");
+            System.out.println("\n  it is not greater than 10,");
         }
 
-        if (num % 2 == 0){
-            System.out.println("\nit is even,");
+        if (num % 2 == 0) {
+            System.out.println("\n  it is even,");
         } else {
-            System.out.println("\nit is odd,");
+            System.out.println("\n  it is odd,");
         }
 
-        switch (num){
+        switch (num) {
             case 2:
-                System.out.println("\nit is the atomic number of the noble gas Helium.");
+                System.out.println("\n  it is the atomic number of the noble gas Helium.");
                 break;
 
             case 10:
-                System.out.println("\nit is the atomic number of the noble gas Neon.");
+                System.out.println("\n  it is the atomic number of the noble gas Neon.");
                 break;
 
             case 18:
-                System.out.println("\nit is the atomic number of the noble gas Argon.");
+                System.out.println("\n  it is the atomic number of the noble gas Argon.");
                 break;
 
             case 36:
-                System.out.println("\nit is the atomic number of the noble gas Krypton.");
+                System.out.println("\n  it is the atomic number of the noble gas Krypton.");
                 break;
 
             case 54:
-                System.out.println("\nit is the atomic number of the noble gas Xenon");
+                System.out.println("\n  it is the atomic number of the noble gas Xenon");
                 break;
 
             case 86:
-                System.out.println("\nit is the atomic number of the noble gas Radon.");
+                System.out.println("\n  it is the atomic number of the noble gas Radon.");
                 break;
 
             case 118:
-                System.out.println("\nit is the atomic number of the noble gas Oganesson.");
+                System.out.println("\n  it is the atomic number of the noble gas Oganesson.");
                 break;
 
             default:
-                System.out.println("\nit is not the atomic number of a noble gas.");
+                System.out.println("\n  it is not the atomic number of a noble gas.");
         }
 
     }
 
-    public static void favChar(char a){
+    public static void favChar(char a) {
         String vowels = "aeiou";
         String str = Character.toString(a);
         String properNum;
         int index = 0;
 
-
-        if (Character.isDigit(a)){
-            System.out.println("\n" + a + " is a number,");
-        } else if ( Character.isLetter(a) && Character.isUpperCase(a)){
-            System.out.println("\n" + a + " is a uppercase letter,");
-        } else if (Character.isLetter(a) && Character.isLowerCase(a)){
-            System.out.println("\n" + a + " is a lowercase letter,");
+        if (Character.isDigit(a)) {
+            System.out.println("\n  " + a + " is a number,");
+            index = (int)a - (int)'0' + 1;
+        } else if ( Character.isLetter(a) && Character.isUpperCase(a)) {
+            System.out.println("\n  " + a + " is a uppercase letter,");
+            index = (int)a - (int)'A' + 1;
+        } else if (Character.isLetter(a) && Character.isLowerCase(a)) {
+            System.out.println("\n  " + a + " is a lowercase letter,");
+            index = (int)a - (int)'a' + 1;
         }
 
-        if (Character.isLetter(a)){
+        if (Character.isLetter(a)) {
             if (!vowels.contains(str.toLowerCase())) {
-                System.out.println("\nit is not a vowel,");
+                System.out.println("\n  it is not a vowel,");
             } else {
-                System.out.println("\nit is a vowel,");
+                System.out.println("\n  it is a vowel,");
             }
         }
 
-        System.out.println("\nits ASCII value is " + (int) a + ",");
-
-        if (Character.isLetter(a)){
-            index = (int) a - (int) 'a' + 1;
-        } else if (Character.isDigit(a)) {
-            index = (int) a - (int) '0' + 1;
-
-        }
+        System.out.println("\n  its ASCII value is " + (int)a + ",");
 
         properNum = switch (index) {
             case 1, 11, 21 -> index + "st";
@@ -116,10 +111,9 @@ public class DataFun {
         };
 
         if (Character.isLetter(a)) {
-            System.out.println("\nand it is the " + properNum + " letter of the alphabet.");
+            System.out.println("\n  and it is the " + properNum + " letter of the alphabet.");
         } else if (Character.isDigit(a)){
-            System.out.println("\nand it is the " + properNum + " digit.");
+            System.out.println("\n  and it is the " + properNum + " digit.");
         }
-
     }
 }
